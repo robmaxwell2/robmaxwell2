47d69f740c7e7710c3bdb20ad0f50f9d9145771d
Robert Maxwell <91917101+robmaxwell2@users.noreply.github.com>
2022-04-25 20:03:01 -0600

    Add files via upload

---
+++ Exercise_4.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>PHP Week 1 Ex 3</title>
+</head>
+<body>
+<?php
+$x = 5;
+$y = 5;     
+$result = $x + $y;
+
+echo 'The result is: '. $result;
+    ?>
+</body>
+</html>
